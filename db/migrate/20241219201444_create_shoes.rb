e0ad07a6afe14cbcf1d5a55caff395761d279b15
Your Name <my@email.org>
2024-12-20 20:03:15 +0000

    Changed id to be consistent and correct

--- db/migrate/20241219201444_create_shoes.rb
+++ db/migrate/20241219201444_create_shoes.rb
@@ -1,7 +1,6 @@
 class CreateShoes < ActiveRecord::Migration[7.2]
   def change
-    create_table :shoes do |t|
-      t.integer :id
+    create_table :shoes do |t| # Rails will automatically add a primary key column `id`
       t.string :name
       t.string :brand
       t.integer :size
--- db/schema.rb
+++ db/schema.rb
@@ -1,19 +1,17 @@
-=begin
-Instead of editing this file, please use the migrations feature of Active Record to
-incrementally modify the database, and then regenerate this schema definition.
-
-This file is the source Rails uses to define the schema when running `bin/rails
-db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-be faster and is potentially less error-prone than running all the
-migrations from scratch. Old migrations may fail to apply correctly if those
-migrations use external dependencies or application code.
-
-It's strongly recommended that you check this file into the version control system.
-=end
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_12_19_201444) do
   create_table "shoes", force: :cascade do |t|
-    t.integer "id"
     t.string "name"
     t.string "brand"
     t.integer "size"
