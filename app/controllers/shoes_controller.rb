31e0934f227fb412dbec6231b0589a7095014ae0
Your Name <my@email.org>
2024-12-20 19:37:42 +0000

    Updated schema, seed and db while fixing unique id.

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,27 +1,10 @@
 class ShoesController < ApplicationController
-  # Action to list all shoes
-  def index
-    @shoes = Shoe.all
-  end
-
-  # Action to show a single shoe
   def show
-    @shoe = Shoe.find(params[:id])
+    @shoe = Shoe.find_by(shoe_id: params[:id]) # Use `shoe_id`
   end
 
-  # Action to create a new shoe
-  def create
-    @shoe = Shoe.new(shoe_params)
-    if @shoe.save
-      redirect_to @shoe, notice: 'Shoe was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
-
-  # Action to update an existing shoe
   def update
-    @shoe = Shoe.find(params[:id])
+    @shoe = Shoe.find_by(shoe_id: params[:id]) # Use `shoe_id`
     if @shoe.update(shoe_params)
       redirect_to @shoe, notice: 'Shoe was successfully updated.'
     else
@@ -29,17 +12,9 @@ class ShoesController < ApplicationController
     end
   end
 
-  # Action to delete a shoe
   def destroy
-    @shoe = Shoe.find(params[:id])
+    @shoe = Shoe.find_by(shoe_id: params[:id]) # Use `shoe_id`
     @shoe.destroy
     redirect_to shoes_path, notice: 'Shoe was successfully deleted.'
-  end
-
-  private
-
-  # Strong parameters to permit only allowed attributes
-  def shoe_params
-    params.require(:shoe).permit(:name, :brand, :size, :price, :stock)
   end
 end
